ed93470e31236386ad16da2a4ad5eaa93371841e
Geoff Riley <geoffr@geoff-ubuntu.adaso.com>
2019-08-21 23:53:54 +0100

    Updated to use module pattern in library, also developed readme

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 wikitree-javascript-sdk
 =======================
-
 Javascript library to work with the WikiTree API functions.
+
+* 21st August 2019: Updated to use the module pattern and current version of the Cookie Plugin.
 
 ## Prerequisites
 * jQuery 1.10 or higher (may work with lower versions)
-* jQuery Cookie Plugin 1.3.1 or later (https://github.com/carhartl/jquery-cookie)
+* jQuery Cookie Plugin 2.2.1 or later (https://github.com/js-cookie/js-cookie)
 
 ## Usage
-
 ````javascript
-
 // Load scripts
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="jquery.cookie.js"></script>
+<script src="//cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 <script src="wikitree.js"></script>
 
 <script type="text/javascript">
@@ -29,13 +28,88 @@ Javascript library to work with the WikiTree API functions.
 	});
 
 </script>
-
+````
 ## Example
 
 The documentation here is incomplete as the SDK (and the API itself) are all in early development.
 The index.html file has a decent example of usage. 
 
-A hosted version is at: http://apps.wikitree.com/apps/casey1/wikitree-javascript-sdk/
+A hosted version is at: http://apps.wikitree.com/apps/riley9287/wikitree-javascript-sdk/
+
+## Objects
+### Wikitree object
+This is a singleton instance intended to contain the current session details.
+
+#### Methods
+* `getinstance()` *class* method to retrieve a pointer to the allocated instance.
+* `create_instance()` **private** *class* method to create an initial instance: only called once, automatically, upon first use of the method `getinstance()`
+* `init()` initialises the singleton instance of the Wikitree object.
+
+#### Properties
+* `instance` **private** property holding the only instance of the Wikitree object.
+* `API_URL` location of the api protocol socket.
+* `session` default session, see `Session` object.
+
+### Session object
+This object manages the logging in and authorisation for communications with the api.
+
+#### Methods
+* `checkLogin({user_id: nnn, user_name: 'AAA-nnn'})` 
+  * performs a `login` using `user_id`.
+    * On success: if the `user_id` is valid, sets the browser cookies and the logged in flag.
+    * On fail: if the `user_id` is invalid, clears the browser cookies and the logged in flag.
+    * On error: clears the browser cookies and the logged in flag.
+* `login({email: 'email@address', password: 'password'})`
+  * performs a `login` using `email` and `password`.
+    * On success: saves `username` and `userid`, sets the browser cookies and the logged in flag.
+    * On fail: **sets** the browser cookies but **clears** the logged in flag.
+    * On error: clears the browser cookies, the logged in flag and the stored `username` and `userid`, also saves the status in `error`.
+* `clientLogin({authcode: 'authcode'})`
+  * performs a `clientLogin` using the `authcode` returned from an external login process.
+    * On success: saves `username` and `userid`, sets the browser cookies and the logged in flag.
+    * On fail: **sets** the browser cookies but **clears** the logged in flag.
+    * On error: clears the browser cookies, the logged in flag and the stored `username` and `userid`, also saves the status in `error`.
+* `logout()`
+  * no communication with service, just clears all identification elements and clears the logged in flag.
+
+#### Properties
+* `user_id` **read only** return stored `user_id` (numeric).
+* `user_name` **read only** return stored `user_name` (string).
+* `loggedIn` **read only** returns logged in status (boolean).
+* `error` **read only** return last stored error status (string).
+* `_user_id` **private** internal storage.
+* `_user_name` **private** internal storage.
+* `_loggedIn` **private** internal storage.
+* `_error` **private** internal storage.
+
+### Person object
+Controls the communication with the api for the loading of person information.
+
+#### Methods
+* `load({fields: 'list of fields to load'})`
+  * performs a `getPerson` request using either the passed field list, or a default field list ('Id,Name,FirstName,MiddleName,LastNameAtBirth,LastNameCurrent,BirthDate,DeathDate,Father,Mother').
+    * On success: stores any returned person records in the person array.
+    * On error: sets the status to reflect any returned error.
+* `person('fieldname')`
+  * looks up a specific field in the person array and returns the appropriate value, if field does not exist then an empty string is returned.
 
 
-````
+#### Properties
+* `Id` **read only** return the value of this field in the person array.
+* `Name` **read only** return the value of this field in the person array.
+* `Gender` **read only** return the value of this field in the person array.
+* `FirstName` **read only** return the value of this field in the person array.
+* `MiddleName` **read only** return the value of this field in the person array.
+* `LastNameAtBirth` **read only** return the value of this field in the person array.
+* `LastNameCurrent` **read only** return the value of this field in the person array.
+* `BirthDate` **read only** return the value of this field in the person array.
+* `DeathDate` **read only** return the value of this field in the person array.
+* `Father` **read only** return the value of this field in the person array.
+* `Mother` **read only** return the value of this field in the person array.
+* `Parents` **read only** return the value of this field in the person array.
+* `Children` **read only** return the value of this field in the person array.
+* `Siblings` **read only** return the value of this field in the person array.
+* `Spouses` **read only** return the value of this field in the person array.
+
+If any fields are required that are not in the above list, they can be obtained by using the `person()` method.
+
--- wikitree.js
+++ wikitree.js
@@ -1,250 +1,332 @@
-/* ======================================================================
- *
- *
- *
- *
- * ======================================================================
- */
+/*
+ ======================================================================
 
+ OBJECT: Wikitree
 
-window.wikitree = window.wikitree || {};
+ ======================================================================
+*/
+const Wikitree = (function () {
+	'use strict';
+	var instance;
+
+	function createInstance() {
+		return {};
+	}
+
+	return {
+		getInstance: function () {
+			if (!instance) {
+				instance = createInstance();
+			}
+			return instance;
+		}
+	}
+})();
+
+wikitree = Wikitree.getInstance();
 wikitree.API_URL = '/api.php';
 
+/*
+ ======================================================================
 
-/* ======================================================================
- *
- * Session
- *
-/* ======================================================================
+ SUPPORT functions
+
+ ======================================================================
  */
+const getCookieDef = (name, def) => {
+	var ret = Cookies.get(name);
 
+	if (typeof ret === "undefined")
+		ret = def;
 
-// Create functional object "class" 
-wikitree.Session = function(opts) {
-	this.user_id    = (opts && opts.user_id) ? opts.user_id : $.cookie('wikitree_wtb_UserID');
-	this.user_name  = (opts && opts.user_name) ? opts.user_name : $.cookie('wikitree_wtb_UserName');
-	this.loggedIn  = false;
+	return ret;
 };
-	
-// Define new method for Session objects to check the current login.
-// Return a promise object (from our .ajax() call) so we can do things when this resolves.
-wikitree.Session.prototype.checkLogin = function (opts){
 
-	var self = this;
+/*
+ ======================================================================
 
-	if (opts && opts.user_id) { self.user_id = opts.user_id; }
-	if (opts && opts.user_name) { self.user_name = opts.user_name; }
+ OBJECT: Session
 
+ ======================================================================
+*/
+const Session = function (opts) {
+	'use strict';
+	var _user_id;
+	var _user_name;
+	var _loggedIn;
+	var _error;
 
-	var request = $.ajax({
-		url: wikitree.API_URL,
-		crossDomain: true,
-		xhrFields: { withCredentials: true },
-		type: 'POST',
-		dataType: 'json',
-		data: { 'action': 'login', 'user_id': self.user_id },
+	(function (opts) {
+		_user_id = parseInt((opts && opts.user_id) ? opts.user_id : getCookieDef('wikitree_wtb_UserID', 0), 10);
+		_user_name = (opts && opts.user_name) ? opts.user_name : getCookieDef('wikitree_wtb_UserName', '');
+		_loggedIn = false;
+		_error = '';
+	})(opts);
 
-		// Local success handling to set our cookies.
-		success: function(data) { 
-			if (data.login.result == self.user_id) { 
-				$.cookie('wikitree_wtb_UserID', self.user_id);
-				$.cookie('wikitree_wtb_UserName', self.user_name);
-				self.loggedIn = true;
-			} else { 
-				//$.cookie('wikitree_wtb_UserID', '');
-				//$.cookie('wikitree_wtb_UserName', '');
-				self.loggedIn = false;
-			}
-		}, 
-		error: function(xhr, status) { 
-			$.cookie('wikitree_wtb_UserID', '');
-			$.cookie('wikitree_wtb_UserName', '');
-			self.loggedIn = false;
+	// Method for Session objects to check the current login.
+	// Return a promise object (from our .ajax() call) so we can do things when this resolves.
+	var checkLogin = function (opts) {
+		if (opts && opts.user_id) {
+			_user_id = opts.user_id;
 		}
-	});
-
-	return request.promise();
-
-}
-	
-
-// Do an actual login through the server API with an Ajax call. 
-// This is for bots where the script has a built-in/known username/password.
-wikitree.Session.prototype.login = function(opts) {
-	var self = this;
-
-	var email    = (opts && opts.email) ? opts.email : '';
-	var password = (opts && opts.password) ? opts.password : '';
-
-	var request = $.ajax({
-		url: wikitree.API_URL,
-		crossDomain: true,
-		xhrFields: { withCredentials: true },
-		type: 'POST',
-		dataType: 'json',
-		data: { 'action': 'login', 'email': email, 'password': password },
-
-		// On successful POST return, check our data. Note from that data whether the login itself was
-		// successful (setting session cookies if so). Call the user callback function when done.
-		success: function(data) { 
-			if (data.login.result == 'Success') { 
-				self.user_id   = data.login.userid;
-				self.user_name = data.login.username;
-				self.loggedIn = true;
-				$.cookie('wikitree_wtb_UserID', self.user_id);
-				$.cookie('wikitree_wtb_UserName', self.user_name);
-			} else { 
-				this.loggedIn = false;
-				$.cookie('wikitree_wtb_UserID', self.user_id);
-				$.cookie('wikitree_wtb_UserName', self.user_name);
-			}
-		}, 
-
-		// On failed POST/server error, act like a failed login.
-		error: function(xhr, status) { 
-			self.user_id = 0;
-			self.user_name = '';
-			self.loggedin = false;
-			$.cookie('wikitree_wtb_UserID', self.user_id);
-			$.cookie('wikitree_wtb_UserName', self.user_name);
+		if (opts && opts.user_name) {
+			_user_name = opts.user_name;
 		}
-	});
 
-	return request.promise();
-	
-}
-
-
-// Do an actual login through the server API with an Ajax call. 
-// This is for interactive apps where we sent the user to wikitree.com to login and got
-// back an auth code to use for login here.
-wikitree.Session.prototype.clientLogin = function(opts) {
-	var self = this;
-
-	var authcode = (opts && opts.authcode) ? opts.authcode : '';
-
-	var request = $.ajax({
-		url: wikitree.API_URL,
-		crossDomain: true,
-		xhrFields: { withCredentials: true },
-		type: 'POST',
-		dataType: 'json',
-		data: { 'action': 'clientLogin', 'authcode': authcode },
-
-		// On successful POST return, check our data. Note from that data whether the login itself was
-		// successful (setting session cookies if so). Call the user callback function when done.
-		success: function(data) { 
-			if (data.clientLogin.result == 'Success') { 
-				self.user_id   = data.clientLogin.userid;
-				self.user_name = data.clientLogin.username;
-				self.loggedIn = true;
-				$.cookie('wikitree_wtb_UserID', self.user_id, {'path': '/'});
-				$.cookie('wikitree_wtb_UserName', self.user_name, {'path': '/'});
-			} else { 
-				self.loggedIn = false;
-				self.error = data.error;
-				$.cookie('wikitree_wtb_UserID', '', {'path': '/'});
-				$.cookie('wikitree_wtb_UserName', '', {'path': '/'});
+		var request = $.ajax({
+			url: wikitree.API_URL,
+			crossDomain: true,
+			xhrFields: {withCredentials: true},
+			type: 'POST',
+			dataType: 'json',
+			data: {'action': 'login', 'user_id': _user_id},
+			// Local success handling to set our cookies.
+			success: (data) => {
+				if (data.login.result === _user_id) {
+					Cookies.set('wikitree_wtb_UserID', _user_id, {'path': '/'});
+					Cookies.set('wikitree_wtb_UserName', _user_name, {'path': '/'});
+					_loggedIn = true;
+				} else {
+					Cookies.remove('wikitree_wtb_UserID', {'path': '/'});
+					Cookies.remove('wikitree_wtb_UserName', {'path': '/'});
+					_loggedIn = false;
+				}
+			},
+			error: (xhr, status) => {
+				Cookies.remove('wikitree_wtb_UserID', {'path': '/'});
+				Cookies.remove('wikitree_wtb_UserName', {'path': '/'});
+				_loggedIn = false;
+				_error = status;
 			}
-		}, 
+		});
 
-		// On failed POST/server error, act like a failed login.
-		error: function(xhr, status) { 
-			self.user_id = 0;
-			self.user_name = '';
-			self.loggedin = false;
-			$.cookie('wikitree_wtb_UserID', '', {'path': '/'});
-			$.cookie('wikitree_wtb_UserName', '', {'path': '/'});
+		return request.promise();
+	};
+
+	// Do an actual login through the server API with an Ajax call.
+	// This is for bots where the script has a built-in/known username/password.
+	var login = function (opts) {
+		var email = (opts && opts.email) ? opts.email : '';
+		var password = (opts && opts.password) ? opts.password : '';
+		var request = $.ajax({
+			url: wikitree.API_URL,
+			crossDomain: true,
+			xhrFields: {withCredentials: true},
+			type: 'POST',
+			dataType: 'json',
+			data: {'action': 'login', 'email': email, 'password': password},
+
+			// On successful POST return, check our data. Note from that data whether the login itself was
+			// successful (setting session cookies if so). Call the user callback function when done.
+			success: (data) => {
+				if (data.login.result === 'Success') {
+					_user_id = data.login.userid;
+					_user_name = data.login.username;
+					_loggedIn = true;
+					Cookies.set('wikitree_wtb_UserID', _user_id);
+					Cookies.set('wikitree_wtb_UserName', _user_name);
+				} else {
+					_loggedIn = false;
+					Cookies.set('wikitree_wtb_UserID', _user_id);
+					Cookies.set('wikitree_wtb_UserName', _user_name);
+				}
+			},
+
+			// On failed POST/server error, act like a failed login.
+			error: (xhr, status) => {
+				_user_id = 0;
+				_user_name = '';
+				_loggedIn = false;
+				_error = status;
+				Cookies.set('wikitree_wtb_UserID', _user_id);
+				Cookies.set('wikitree_wtb_UserName', _user_name);
+			}
+		});
+
+		return request.promise();
+	};
+
+	// Do an actual login through the server API with an Ajax call.
+	// This is for interactive apps where we sent the user to wikitree.com to login and got
+	// back an auth code to use for login here.
+	var clientLogin = function (opts) {
+		var authcode = (opts && opts.authcode) ? opts.authcode : '';
+		var request = $.ajax({
+			url: wikitree.API_URL,
+			crossDomain: true,
+			xhrFields: {withCredentials: true},
+			type: 'POST',
+			dataType: 'json',
+			data: {'action': 'clientLogin', 'authcode': authcode},
+
+			// On successful POST return, check our data. Note from that data whether the login itself was
+			// successful (setting session cookies if so).
+			success: (data) => {
+				//console.log(data);
+				if (data.clientLogin.result === 'Success') {
+					_user_id = data.clientLogin.userid;
+					_user_name = data.clientLogin.username;
+					_loggedIn = true;
+					Cookies.set('wikitree_wtb_UserID', _user_id, {'path': '/'});
+					Cookies.set('wikitree_wtb_UserName', _user_name, {'path': '/'});
+				} else {
+					_loggedIn = false;
+					Cookies.set('wikitree_wtb_UserID', '', {'path': '/'});
+					Cookies.set('wikitree_wtb_UserName', '', {'path': '/'});
+				}
+			},
+
+			// On failed POST/server error, act like a failed login.
+			error: (xhr, status) => {
+				_user_id = 0;
+				_user_name = '';
+				_loggedIn = false;
+				_error = status;
+				Cookies.remove('wikitree_wtb_UserID', {'path': '/'});
+				Cookies.remove('wikitree_wtb_UserName', {'path': '/'});
+			}
+		});
+
+		return request.promise();
+	};
+
+	var logout = function () {
+		_loggedIn = false;
+		_user_id = 0;
+		_user_name = '';
+		_error = '';
+		Cookies.remove('wikitree_wtb_UserID', {'path': '/'});
+		Cookies.remove('wikitree_wtb_UserName', {'path': '/'});
+	};
+
+	return {
+		checkLogin: checkLogin,
+		login: login,
+		clientLogin: clientLogin,
+		logout: logout,
+		get user_id() {
+			return _user_id;
+		},
+		get user_name() {
+			return _user_name;
+		},
+		get loggedIn() {
+			return _loggedIn;
+		},
+		get error() {
+			return _error;
 		}
-	});
+	}
+};
 
-	return request.promise();
-	
-}
-
-wikitree.Session.prototype.logout = function(opts) {
-	this.loggedIn = false;
-	this.user_id = 0;
-	this.user_name = '';
-	$.removeCookie('wikitree_wtb_UserID');
-	$.removeCookie('wikitree_wtb_UserName');
-}
-
-
-wikitree.init = function(opts) { 
-	wikitree.session = new wikitree.Session();
-}
-
+wikitree.init = function (opts) {
+	wikitree.session = new Session(opts);
+};
 
 
 /*
- * Person
- *
- *
- */
+ ======================================================================
 
+ OBJECT: Person
 
-// wikitree.Person( opts )
-wikitree.Person = function(opts){
-	this.user_id = (opts && opts.user_id) ? opts.user_id : 0;
-	this.loaded = false;
-	this.loading = false;
-};
+ ======================================================================
+*/
 
-// wikitree.Person.load(callback, [fields])
-// 	The API on the server is called with $.ajax/$.post functions from jQuery. Those calls are *asynchronous*. 
-wikitree.Person.prototype.load = function(opts){
-	var self = this;
+const Person = function (opts) {
+	'use strict';
+	var _people;
+	var _user_id;
+	var _loaded;
+	var _loading;
+	var _status;
 
-	// If we have a fields passed in, use those. If not, use a default set.
-	var fields = 'Id,Name,FirstName,MiddleName,LastNameAtBirth,LastNameCurrent,BirthDate,DeathDate,Father,Mother'; 
-	if (opts && opts.fields) { fields = opts.fields; }
+	(function (opts) {
+		_user_id = (opts && opts.user_id) ? opts.user_id : 0;
+		_loaded = false;
+		_loading = false;
+		_status = '';
+		_people = {};
+	})(opts);
 
-
-	// If this Person is already loaded, we're all done. If not, we (may) have work to do.
-	if (!self.loaded) {
-		// Javascript will run right through the .ajax() call below and it's possible this Person.load() function will get called 
-		// again before loaded = true and before the first .ajax() call has returned. We don't want to post to the server more than once.
-		// If we're loading already, we don't have anything new to do.
-		if (!self.loading) { 
-			// Start loading our content from the server API.
-			self.loading = true;
-
-			// Post our content to the server API, passing along the requested fields.
-			// Use crossDomain=true in case we end up hosting this on something like apps.wikitree.com but configured
-			// to query the live database/API at www.wikitree.com. 
-			var request = $.ajax({
-				url: wikitree.API_URL,
+	const _doLoad = function (opts) {
+		// If we have a fields passed in, use those. If not, use a default set.
+		var fields = 'Id,Name,FirstName,MiddleName,LastNameAtBirth,LastNameCurrent,BirthDate,DeathDate,Father,Mother';
+		if (opts && opts.fields) {
+			fields = opts.fields;
+		}
+		// Post our content to the server API, passing along the requested fields.
+		// Use crossDomain=true in case we end up hosting this on something like apps.wikitree.com but configured
+		// to query the live database/API at www.wikitree.com.
+		var request = $.ajax(
+			wikitree.API_URL,
+			{
 				crossDomain: true,
-				xhrFields: { withCredentials: true }, 
+				xhrFields: {withCredentials: true},
 				type: "POST",
 				dataType: 'json',
-				data: { 'action': 'getPerson', 'key': self.user_id, 'fields': fields, 'format': 'json' },
-	
+				data: {'action': 'getPerson', 'key': _user_id, 'fields': fields, 'format': 'json'},
+
 				// On success, we note that we're done loading. If we got data back, we store it in self and set loaded=true.
-				success: function(data) { 
-					self.loading = false;
-					self.status = data[0].status;
-					if (!self.status) { 
-						for (var x in data[0].person) { 
-							self[x] = data[0].person[x];
+				success: (data) => {
+					_loading = false;
+					_status = data[0].status;
+					if (!_status) {
+						for (var x in data[0].person) {
+							_people[x] = data[0].person[x];
 						}
-					} else { 
-						self.user_id = 0;
+					} else {
+						_user_id = 0;
 					}
-					self.loaded = true;
+					_loaded = true;
 				},
-	
+
 				// On error, report the "status" we got back.
-				error: function(xhr, status) { 
-					self.loading = false;
-					self.loaded  = false;
-					self.status = 'Error in API query';
+				error: (xhr, status) => {
+					_loading = false;
+					_loaded = false;
+					_status = 'Error in API query ' + status;
 				}
 			});
 
-			return request.promise();
-		}
-	} 
+		return request.promise();
+	};
 
+	const load = function (opts) {
+		// If this Person is already loaded, we're all done. If not, we (may) have work to do.
+		if (!_loaded) {
+			// Javascript will run right through the _doLoad() call below and it's possible this load() function will
+			// get called again before loaded = true and before the first .ajax() call has returned.
+			// We don't want to post to the server more than once.
+			// If we're loading already, we don't have anything new to do.
+			if (!_loading) {
+				// Start loading our content from the server API.
+				_loading = true;
+				return _doLoad(opts);
+			}
+		}
+	};
+
+	const person = function (x) {
+		return (x in _people) ? _people[x] : '';
+	};
+
+	return {
+		load: load,
+		person: person,
+		get Id() {return person('Id')},
+		get Name() {return person('Name')},
+		get Gender() {return person('Gender')},
+		get FirstName() {return person('FirstName')},
+		get MiddleName() {return person('MiddleName')},
+		get LastNameAtBirth() {return person('LastNameAtBirth')},
+		get LastNameCurrent() {return person('LastNameCurrent')},
+		get BirthDate() {return person('BirthDate')},
+		get DeathDate() {return person('DeathDate')},
+		get Father() {return person('Father')},
+		get Mother() {return person('Mother')},
+		get Parents() {return person('Parents')},
+		get Children() {return person('Children')},
+		get Siblings() {return person('Siblings')},
+		get Spouses() {return person('Spouses')}
+	}
 };
